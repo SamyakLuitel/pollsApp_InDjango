3f929850e0b730f29efb5ba3608d067e8260d764
Samk <samkotaku@gmail.com>
2021-01-23 09:58:01 +0545

    optimized poll app

---
+++ mysite/home/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+#HttpRequest.COOKIES (A dictionary containing all cookies. Keys and values are strings.)
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,18 +1,29 @@
-from django.urls import path 
-from . import views 
+'''
+from django.urls import path
 
+from . import views
 
-app_name = 'polls'
-
+#Namespaceing URL names 
+app_name ='polls'
 urlpatterns = [
-    # ex: /
     path('', views.index, name='index'),
-    # ex: /5/
-    path('polls/<int:question_id>/', views.detail, name='detail'),
-    # ex: /5/results/
-    path('polls/<int:question_id>/results/', views.results, name='results'),
-    # ex: /5/vote/
-    path('polls/<int:question_id>/vote/', views.vote, name='vote'),
+    path('owner',views.owner, name="owner"),
+    path('<int:question_id>/',  views.detail, name ="detail"),
+    path('<int:question_id>/results/',views.results, name="results"),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
 
+'''
 
+from django.urls import path
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('owner',views.owner, name="owner"),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
