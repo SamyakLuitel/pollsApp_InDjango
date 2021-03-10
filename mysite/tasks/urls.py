7f0a1e696244e1a6ed330fcd7571efeeba51baec
Samk <samkotaku@gmail.com>
2021-03-10 12:48:29 +0545

    todo app

--- mysite/tasks/urls.py
+++ mysite/tasks/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 from . import views
 from django.views.generic import TemplateView
+from django.contrib import admin
 
-
+app_name = 'tasks'
 urlpatterns = [
     #path('', TemplateView.as_view(template_name='maps/mapbox.html')),
     path('', views.index, name='todo'),
+    path('admin/', admin.site.urls),
+    path('update_task/<str:pk>/', views.updateTask, name="update_task"),
+    path('delete/<str:pk>/', views.deleteTask, name="delete"),
+
+
+
+
 
 ]
